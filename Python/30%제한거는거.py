8e3c147fb566a3bba71d9bb1b51350bbb9296633
pray <thddnr9508@gmail.com>
2024-02-05 19:24:50 +0900

    "ss"

--- Python/30%제한거는거.py
+++ Python/30%제한거는거.py
@@ -17,12 +17,17 @@ samsung=pd.read_csv(path+"삼성 240205.csv",encoding='euc-kr',index_col=0)     
 amore=pd.read_csv(path+"아모레 240205.csv",encoding='euc-kr',index_col=0)       #아모레 데이터수 4350
 y1=samsung['시가']
 y2=amore['종가']
-y1=y1[1:1419]
-y2=y2[1:1419]
-samsung= samsung[1:1419]
-amore= amore[1:1419]
-
-# print(samsung.columns)
+y1=y1[0:1419]
+y2=y2[0:1419]
+samsung= samsung[0:1419]
+amore= amore[0:1419]
+samsung=samsung.sort_values(['일자'],ascending=[True])
+amore=amore.sort_values(['일자'],ascending=[True])
+y1=y1.sort_values(ascending=[True])
+y2=y2.sort_values(ascending=[True])
+z=samsung['시가']
+# print(np.unique(z,return_counts=True))
+print(z)
 
 # 일부 컬럼만 변경
 samsung.rename(columns=lambda x: '전일비투' if x == 'Unnamed: 6' else x, inplace=True)
@@ -61,8 +66,8 @@ amore=amore.astype(np.float32)
 y1=y1.astype(np.float32)
 y2=y2.astype(np.float32)
 
-size = 5
-pred_step = 1
+size = 10
+pred_step = 2
 
 
 def split_xy(data, time_step, y_col, pred_step):
@@ -84,7 +89,9 @@ x2, y2 = split_xy(amore, size, '종가', pred_step)
 # y = samsung['시가'][0:4350].astype(float)
 print(x1.shape,x2.shape,y1.shape,y2.shape)  #(1387, 30, 16) (1387, 30, 16) (1387,) (1387,)
 
-x1_train,x1_test,x2_train,x2_test,y1_train,y1_test,y2_train,y2_test=train_test_split(x1,x2,y1,y2,train_size=0.9,random_state=3)
+x1_train,x1_test,x2_train,x2_test,y1_train,y1_test,y2_train,y2_test=train_test_split(x1,x2,y1,y2,train_size=0.9,random_state=3
+                                                                                     ,shuffle=True
+                                                                                     )
 # print(x1_train.shape,x2_train.shape,y_train.shape)
 x1_train=x1_train.reshape(-1,size*16)
 x1_test=x1_test.reshape(-1,size*16)
@@ -104,56 +111,42 @@ x2_test=x2_test.reshape(-1,size,16)
 
 # 모델1
 input1 = Input(shape= (size,16,))
-dense1= Conv1D(100,2,activation='swish')(input1)
-drop1=Dropout(0.5)(dense1)
+dense1= Conv1D(100,2,activation='swish',padding='same')(input1)
+drop1=Dropout(0.2)(dense1)
 dense2= Conv1D(200,2,activation='swish')(drop1)
-drop2=Dropout(0.5)(dense2)
+drop2=Dropout(0.2)(dense2)
 dense3= Conv1D(300,2,activation='swish')(drop2)
-drop3=Dropout(0.5)(dense3)
+drop3=Dropout(0.2)(dense3)
 dense4= Conv1D(400,2,activation='swish')(drop3)
 flatten= Flatten()(dense4)
 output1= Dense(10,activation='swish')(flatten)
 
 # 모델2
 input11 = Input(shape= (size,16,))
-dense2_1= Conv1D(100,2,activation='swish')(input11)
-drop2_1=Dropout(0.5)(dense2_1)
+dense2_1= Conv1D(100,2,activation='swish',padding='same')(input11)
+drop2_1=Dropout(0.2)(dense2_1)
 dense2_2= Conv1D(200,2,activation='swish')(drop2_1)
-drop2_2=Dropout(0.5)(dense2_2)
+drop2_2=Dropout(0.2)(dense2_2)
 dense2_3= Conv1D(200,2,activation='swish')(dense2_2)
-drop2_3=Dropout(0.5)(dense2_3)
+drop2_3=Dropout(0.2)(dense2_3)
 dense2_4= Conv1D(300,2,activation='swish')(dense2_3)
 flatten= Flatten()(dense2_4)
 output11= Dense(10,activation='swish')(flatten)
 
 merge1 = concatenate([output1,output11])
 merge2 = Dense(100,activation='swish')(merge1)
-merge3 = Dense(110,activation='swish')(merge2)
-merge4 = Dense(110,activation='swish')(merge3)
-merge5 = Dense(110,activation='swish')(merge4)
-merge6 = Dense(110,activation='swish')(merge5)
-merge7 = Dense(110,activation='swish')(merge6)
-merge8 = Dense(110,activation='swish')(merge7)
+merge3 = Dense(200,activation='swish')(merge2)
+merge4 = Dense(250,activation='swish')(merge3)
+merge5 = Dense(300,activation='swish')(merge4)
+merge6 = Dense(250,activation='swish')(merge5)
+merge7 = Dense(200,activation='swish')(merge6)
+merge8 = Dense(150,activation='swish')(merge7)
 merge9 = Dense(100,activation='swish')(merge8)
 last_output1 = Dense(1, name='last')(merge9)
 last_output2 = Dense(1, name='last2')(merge6)
 
 # 모델 정의 후에 compile 호출이 필요합니다.
 model = Model(inputs=[input1,input11],outputs=[last_output1,last_output2])
-
-initial_learning_rate = 0.001
-adam_optimizer = Adam(learning_rate=initial_learning_rate)
-def lr_schedule(epoch):
-    """
-    에포크마다 학습률을 감소시키는 함수
-    """
-    drop_rate = 0.5
-    epochs_drop = 10  # 몇 번의 에포크마다 학습률을 감소시킬 것인지 설정
-    new_learning_rate = initial_learning_rate * np.power(drop_rate, np.floor((1 + epoch) / epochs_drop))
-    return new_learning_rate
-
-# 학습률 스케줄러 콜백 생성
-lr_scheduler = LearningRateScheduler(lr_schedule)
 
 from keras.callbacks import EarlyStopping,ModelCheckpoint,Callback
 
@@ -193,36 +186,49 @@ class ThresholdCallback(Callback):
 
 # Callback 인스턴스 생성
 threshold_callback = ThresholdCallback(threshold_percentage=10.0)    #수치조정
-es= EarlyStopping(monitor='val_loss',mode='auto',patience=70,verbose=1,restore_best_weights=True)
+es= EarlyStopping(monitor='val_loss',mode='auto',patience=150,verbose=1,restore_best_weights=True)
 mcp = ModelCheckpoint(
     monitor='val_loss',
     mode='auto',
     verbose=1,
     save_best_only=True,
-    filepath='c:/_data/si_hum/삼성전자1.hdf5'
+    filepath='c:/_data/si_hum/삼성전자3.hdf5'
     )
 # 모델에 콜백 등록
 model.compile(loss='mae',optimizer='adam')
 model.fit([x1_train, x2_train], [y1_train, y2_train], epochs=2000, batch_size=100, validation_split=0.1, verbose=2,
-          callbacks=[es, mcp, threshold_callback,
-                     #lr_scheduler
-                     ])
+          callbacks=[es, mcp, threshold_callback])
 
-model.save("c:/_data/si_hum/삼성전자1.h5")
+model.save("c:/_data/si_hum/삼성전자3.h5")
 #4.결과예측
 result = model.evaluate([x1_test,x2_test],[y1_test,y2_test])
 predict = model.predict([x1_test,x2_test])
 
 predict = np.round(predict,2)
-print("mse:",result)
-# print("mse:",result[0])
-# print("mae:",result[3])
-print("삼성:",predict[0][0])
-print("아모레:",predict[1][0])
+print("mae:",result)
+print("삼성:",predict[0][-1])
+print("아모레:",predict[1][-1])
 # print("예측값:",predict)
+#삼성74,200 아모레120,400
 
 '''
-mse: [20747.25, 14833.8798828125, 5913.36865234375]
-삼성: [80948.74]
-아모레: [241743.1]
+2018/05/03    2,650,000
+2018/05/04       53,000
+2018/05/08       52,600
+2018/05/09       52,600
+2018/05/10       51,700
+                ...
+2024/01/29       73,800
+2024/01/30       75,000
+2024/01/31       73,400
+2024/02/01       73,000
+2024/02/02       74,000
+
+mse: [7065.3203125, 1493.4793701171875, 5571.84130859375]
+삼성: [73564.28]
+아모레: [127007.56] 삼성전자1번
+
+mae: [7379.51416015625, 1826.3887939453125, 5553.12548828125]
+삼성: [74874.89]
+아모레: [128233.11] 2번
 '''
