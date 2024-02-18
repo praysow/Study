31f9d2763cb59f3349275cfa5cbd422daca51af8
parysow <thddnr9508@gmail.com>
2024-02-18 21:10:14 +0900

    "ss"

--- ml/m03_01_iris 전부for문으로 고치기.py
+++ ml/m03_01_iris 전부for문으로 고치기.py
@@ -2,9 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import Perceptron,LogisticRegression
-from sklearn.linear_model import LogisticRegression,LinearRegression
 from sklearn.neighbors import KNeighborsClassifier,KNeighborsRegressor
 from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
 from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
